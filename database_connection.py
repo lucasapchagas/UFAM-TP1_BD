74ede47767f0cf84a58d2b5bf96a8535847a8651
Lucas Chagas <lucas.chagas@icomp.ufam.edu.br>
2023-12-18 21:53:51 -0400

    create_database function

--- database_connection.py
+++ database_connection.py
@@ -1,42 +1,50 @@
-
 import psycopg2
 from psycopg2 import OperationalError
 import configparser
 
-# Definindo constantes públicas para o status da conexão
-CONNECTION_SUCCESS = "Conexão bem-sucedida"
-CONNECTION_FAILURE = "Erro ao conectar ao banco de dados"
+FALHA_CONEXAO = "ERRO AO CONECTAR AO BANCO DE DADOS"
+SUCESSO_CONEXAO = "SUCESSO AO CONECTAR AO BANCO DE DADOS"
+SUCESSO_CRIAR_BANCO = "SUCESSO AO CRIAR BANCO DE DADOS"
 
-def connect_to_database():
-    """Tenta estabelecer uma conexão com o banco de dados usando configurações de um arquivo externo."""
-    # Lendo as configurações do arquivo
+def create_connection():
     config = configparser.ConfigParser()
     config.read('db_config.ini')
 
     try:
-        # Conectar ao banco de dados usando as informações do arquivo de configuração
         connection = psycopg2.connect(
-            host=config['DATABASE']['HOST'],
-            database=config['DATABASE']['DBNAME'],
-            user=config['DATABASE']['USER'],
-            password=config['DATABASE']['PASSWORD']
+            host=config['database']['host'],
+            database=config['database']['dbname'],
+            user=config['database']['user'],
+            password=config['database']['password']
         )
-        
-        # Cria um cursor para realizar operações no banco de dados
-        cursor = connection.cursor()
-        
-        # Executar um comando SQL para testar a conexão
+
+        cursor = connection.cursor()        
         cursor.execute("SELECT version();")
-        db_version = cursor.fetchone()
-        
-        # Fechar a conexão
         cursor.close()
-        connection.close()
         
-        return CONNECTION_SUCCESS, db_version
-    
+        return SUCESSO_CONEXAO, connection
     except OperationalError as e:
-        return CONNECTION_FAILURE, str(e)
+        return FALHA_CONEXAO, str(e)
+    
+def create_database(connection, database_name):
+    if (connection[0] == FALHA_CONEXAO):
+        return
+    
+    try:
+        COMANDO_SQL = "CREATE DATABASE {}".format(database_name)
 
+        cursor = connection[1].cursor()
+        
+        connection[1].autocommit = True
+        cursor.execute(COMANDO_SQL)
+        connection[1].autocommit = False
+        cursor.close()
 
-print(connect_to_database())
+        return SUCESSO_CRIAR_BANCO
+    except OperationalError as e:
+        return FALHA_CONEXAO, str(e)
+
+connection = create_connection()
+create_database(connection, "tp1_test")
+
+connection[1].close()
